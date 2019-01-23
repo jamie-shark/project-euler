77751c7da6b06ddd5c2567ac1e1586c7d7b11014
Jon Campbell <jonathan.campbell@firsthelpline.com>
2019-01-23 15:48:17 +0000

    Create factorsOf function

--- highly-divisible-triangular-number.fsx
+++ highly-divisible-triangular-number.fsx
@@ -12,4 +12,19 @@ let triangularNumbers =
         }
     next 0 1
 
+let factorsOf n =
+    let rec helper factors = function
+        | x when x > (n / 2)       -> factors
+        | x when isDivisibleBy x n -> helper (x::factors) (x+1)
+        | x                        -> helper  factors     (x+1)
+    helper (n::[]) 1
+
 triangularNumbers |> Seq.take 10 |> ContainsTheSameItemsAs [1;3;6;10;15;21;28;36;45;55]
+
+factorsOf 1  |> ContainsTheSameItemsAs [1]
+factorsOf 3  |> ContainsTheSameItemsAs [1;3]
+factorsOf 6  |> ContainsTheSameItemsAs [1;2;3;6]
+factorsOf 10 |> ContainsTheSameItemsAs [1;2;5;10]
+factorsOf 15 |> ContainsTheSameItemsAs [1;3;5;15]
+factorsOf 21 |> ContainsTheSameItemsAs [1;3;7;21]
+factorsOf 28 |> ContainsTheSameItemsAs [1;2;4;7;14;28]
