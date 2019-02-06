21584e515aab80ad902dd8f4e2a1744550d0db0e
Jon Campbell <jonathan.campbell@firsthelpline.com>
2019-02-06 17:22:52 +0000

    Solve problem 16

--- power-digit-sum.fsx
+++ power-digit-sum.fsx
@@ -3,6 +3,18 @@
 open Assertions
 open Lib
 
-let powerDigitSum exponent = 26
+let toIntDigits = char >> string >> int
 
+let powerDigitSum exponent =
+    2I ** exponent
+    |> string
+    |> Seq.map toIntDigits
+    |> Seq.sum
+
+
+2I ** 2 |> Is 4I
+2I ** 4 |> Is 16I
+powerDigitSum 2 |> Is 4
+powerDigitSum 4 |> Is 7
 powerDigitSum 15 |> Is 26
+powerDigitSum 1000 |> Is 1366
