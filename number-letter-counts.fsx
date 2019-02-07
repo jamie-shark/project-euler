277cb2025f258c41d57e70cc43a5668a8f134af5
Jon Campbell <jonathan.campbell@firsthelpline.com>
2019-02-07 11:13:14 +0000

    **WIP

--- number-letter-counts.fsx
+++ number-letter-counts.fsx
@@ -6,15 +6,6 @@ open Lib
 type namedNumber = { number: int; name: string }
 
 let words = [
-    { number = 9  ; name = "nine"      }
-    { number = 8  ; name = "eight"     }
-    { number = 7  ; name = "seven"     }
-    { number = 6  ; name = "six"       }
-    { number = 5  ; name = "five"      }
-    { number = 4  ; name = "four"      }
-    { number = 3  ; name = "three"     }
-    { number = 2  ; name = "two"       }
-    { number = 1  ; name = "one"       }
     { number = 90 ; name = "ninety"    }
     { number = 80 ; name = "eighty"    }
     { number = 70 ; name = "seventy"   }
@@ -33,12 +24,37 @@ let words = [
     { number = 12 ; name = "twelve"    }
     { number = 11 ; name = "eleven"    }
     { number = 10 ; name = "ten"       }
+    { number = 9  ; name = "nine"      }
+    { number = 8  ; name = "eight"     }
+    { number = 7  ; name = "seven"     }
+    { number = 6  ; name = "six"       }
+    { number = 5  ; name = "five"      }
+    { number = 4  ; name = "four"      }
+    { number = 3  ; name = "three"     }
+    { number = 2  ; name = "two"       }
+    { number = 1  ; name = "one"       }
 ]
 
-let asWord n =
-    let namedNumber = words |> Seq.find (fun r -> r.number = n)
-    namedNumber.name
+let getTens n =
+    n
+    |> string
+    |> (fun s -> s.Substring((s.Length-2), 2))
+    |> int
 
-asWord 1  |> Is "one"
-asWord 19 |> Is "nineteen"
+getTens 21 |> Is 20
+getTens 2 |> Is 0
+//getLast 21 |> Is 1
+getTens 32 |> Is 30
+//getLast 32 |> Is 2
+
+//let asWord n =
+//    let thousands = getThousands n
+//    let hundreds = getHundreds n
+//    let tens = getTens n
+//    let namedNumber = words |> Seq.find (fun r -> r.number = n)
+//    namedNumber.name
+
+//asWord 1  |> Is "one"
+//asWord 19 |> Is "nineteen"
+//asWord 21 |> Is "twentyone"
 
